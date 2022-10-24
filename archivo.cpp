c9f421bfea065a4fcb7d150c45c98cb233299d91
zusrico <jesusgarciaflt@gmail.com>
2022-10-25 01:47:30 +0200

    Commit inicial 2

--- archivo.cpp
+++ archivo.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main(){
     cout<<"Prueba github 2"<<endl;
+    cout<<"Prueba github commit 2"<<endl;
     return 0;
 }
---
+++ archivo2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <stdlib.h> //new y delete
+#include "cronometro.h"
+#include "orden.h"
+#define TAM_MAX 20000
+
+using namespace std;
+
+int main(){
+    cronometro c;
+    int *vector, j;
+    float tempo;
+
+    for(int j = 1000; j < TAM_MAX; j = j + 1000){
+        vector = new int[j];
+        for(int i = 0; i<j; i++)
+            vector[i] = i+1;
+        random_shuffle(vector, vector+j); 
+
+        c.activar();
+        intercambioDirectivo(vector, j); //aplicar aqui cualquier metodo de ordenacion
+        c.parar();
+
+        tempo = c.tiempo();
+        cout<<"Usando medida directa y metodo de ordenacion directo, se ha tardado: "<<tempo<<endl;
+    }
+
+    return 0;
+}
+
